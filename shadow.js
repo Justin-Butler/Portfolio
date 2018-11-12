5f6835a70000588247dd2acfc9500b2cf3ad9f83
Justin-Butler <42967802+Justin-Butler@users.noreply.github.com>
2018-11-12 10:40:53 -0500

    Files Added
    Portfolio Files added.

---
+++ shadow.js
@@ -0,0 +1,9 @@
+//Hover Effect for each Card.
+$('#card1, #card2, #card3, #card4, #card5, #card6, #card7').on({
+  mouseenter: function(){
+    $(this).css("boxShadow", "2px 2px 10px #ffffff");
+  },
+  mouseleave: function(){
+    $(this).css("boxShadow", "none");
+  }
+});
