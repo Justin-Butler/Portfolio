920b11816ffd93ff80cd47e448557ae4a0861d53
Justin-Butler <42967802+Justin-Butler@users.noreply.github.com>
2018-12-21 11:21:25 -0500

    Update shadow.js

--- shadow.js
+++ shadow.js
@@ -1,5 +1,5 @@
 //Hover Effect for each Card.
-$('#card1, #card2, #card3, #card4, #card5, #card6, #card7, #card8').on({
+$('#card').on({
   mouseenter: function(){
     $(this).css("boxShadow", "2px 2px 10px #ffffff");
   },
