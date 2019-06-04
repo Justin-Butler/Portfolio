a212200c4f32b0e8ad8d058137c604633f2d5f43
Justin-Butler <42967802+Justin-Butler@users.noreply.github.com>
2019-06-04 13:37:02 -0400

    Delete shadow.js
    Removed as it is unnecessary

--- shadow.js
+++
@@ -1,9 +0,0 @@
-//Hover Effect for each Card.
-$('.card').on({
-  mouseenter: function(){
-    $(this).css("boxShadow", "2px 2px 10px #ffffff");
-  },
-  mouseleave: function(){
-    $(this).css("boxShadow", "none");
-  }
-});
